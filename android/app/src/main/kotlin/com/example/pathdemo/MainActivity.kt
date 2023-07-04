0a66984e6f69ccb2a0ea376f35755ad9b2f644ec
RVdobariya <111721478+RVdobariya@users.noreply.github.com>
2023-07-04 15:27:44 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/pathdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pathdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+   _incrementCounter() async {
+    Directory? directory = await getExternalStorageDirectory();
+    String fileName =
+        "xyz.pdf";
+    String newPath = "";
+    debugPrint("directory");
+    List<String> paths = directory!.path.split("/");
+    for (int x = 1; x < paths.length; x++) {
+      String folder = paths[x];
+      if (folder != "Android") {
+        newPath += "/$folder";
+      } else {
+        break;
+      }
+    }
+    newPath = "$newPath/demo";
+    directory = Directory(newPath);
+
+    if (!directory.existsSync()) {
+      debugPrint("ok ok == $directory");
+      print("permission${await Permission.storage.status}");
+      await Permission.storage.request();
+      PermissionStatus status2 = await Permission.manageExternalStorage.request();
+      try {
+        directory.createSync(recursive: false);
+      } on Exception catch (e) {
+        print("errroooor${e}");
+      }
+    }
+    if (await directory.exists()) {
+      debugPrint("ok1 ok1 == $directory");
+      final File file = File("${directory.path}/$fileName");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
